c19bcd63edae4157610b83d10df60dc1210f3e28
Aryan <aryan09ashish@gmail.com>
2025-08-14 14:58:24 +0530

    Update exception.py

--- src/exception.py
+++ src/exception.py
@@ -21,25 +21,3 @@ class CustomException(Exception):
         return self.error_message
     
 
-
-        
-=======
-import sys
-
-def error_message_detail(error,error_detail:sys):
-    _,_,exc_tb=error_detail.exc_info()
-    file_name=exc_tb.tb_frame.f_code.co_filename
-    error_message= "error occured in python script name[{0}] line number[{1}] error message[{2}]".format(
-     file_name,exec_tb.tb_lineno,str(error)
-    return error_message
-
-    )
-
-class CustomException(Exception):
-    def __init__(self,error_message,error_detail:sys):
-        super.__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_detail=error_detail)
-
-    def __str__(self):
-        return self.error_message
->>>>>>> a71b9b4199ac5fb21dbff96da814a41182003ae4
